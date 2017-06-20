e84e98c4a747b20ee1ebb69df639059cb38380f5
Shaun Williams <shaun.e.williams@gmail.com>
2017-06-20 12:36:14 -0500

    1.1.0

--- CHANGES.md
+++ CHANGES.md
@@ -1,3 +1,8 @@
+# 1.1.0
+
+- update to Parinfer 2.2.1
+  - fix: do not process if unmatched close-parens found anywhere
+
 # 1.0.1
 
 - fix `parlinter` binary publish
--- parlinter.js
+++ parlinter.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+// Parlinter 1.1.0
+
 // Source copied from Prettier's CLI!  (MIT License)
 // https://github.com/prettier/prettier/blob/master/bin/prettier.js
 
@@ -13,7 +15,7 @@ const minimist = require("minimist");
 const readline = require("readline");
 const parinfer = require("parinfer");
 
-const version = "0.1.0";
+const version = "1.1.0";
 
 const argv = minimist(process.argv.slice(2), {
   boolean: ["write", "trim", "stdin", "help", "version", "list-different"],
